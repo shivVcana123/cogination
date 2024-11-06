a1509d2e1afaddcac0d9d30f9b9e65d95d115273
supriyaVcana <supriya@vcanaglobal.com>
2024-11-06 18:39:35 +0530

    migration

--- database/migrations/2024_11_06_104532_create_footers_table.php
+++ database/migrations/2024_11_06_104532_create_footers_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('footers', function (Blueprint $table) {
             $table->id();
-
-            $table->string('title');
-
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('footers');
     }
 };
