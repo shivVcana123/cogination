85e3d95ed663a5f90b0f1d18242145ee070e530e
shiv kumar <shiv@vcanaglobal.com>
2024-11-08 15:58:34 +0530

    dd

--- database/migrations/2024_11_06_104524_create_headers_table.php
+++ database/migrations/2024_11_06_104524_create_headers_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
     {
         Schema::create('headers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->string('link')->unique();
-            $table->string('title');
-            $table->string('sub_title');
-            $table->text('title');
-            $table->text('sub_title');   
-            $table->string('title');
-            $table->text('sub_title');
+            $table->foreignId('parent_id')->nullable()->constrained('headers')->onDelete('cascade'); // Self-referencing foreign key
+            $table->string('sub_title')->nullable();
+            $table->string('sub_link')->nullable()->unique();
             $table->timestamps();
         });
     }
