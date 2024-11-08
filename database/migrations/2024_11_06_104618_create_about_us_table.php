4dc81704c2c41e622f408ad1e6700cb6f7a8d12f
shiv kumar <shiv@vcanaglobal.com>
2024-11-08 18:34:43 +0530

    sdsd

--- database/migrations/2024_11_06_104618_create_about_us_table.php
+++ database/migrations/2024_11_06_104618_create_about_us_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('about_us', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->string('subtitle')->nullable();
+            $table->string('description')->nullable();
+            $table->string('button_content')->nullable();
+            $table->string('button_link')->nullable();
+            $table->string('image')->nullable();
+            $table->string('background_color')->nullable();
             $table->timestamps();
         });
     }
