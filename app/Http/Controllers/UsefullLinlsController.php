a24e707aa310da91fc3ae104c6fc014747124869
shiv kumar <shiv@vcanaglobal.com>
2024-11-11 11:48:36 +0530

    work apis

---
+++ app/Http/Controllers/UsefullLinlsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsefullLinlsController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HeaderController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LatestNewsController;
+use App\Http\Controllers\ServicesController;
+use App\Http\Controllers\UsefullLinlsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,6 +34,19 @@ Route::get('/home/{id?}', [HomeController::class, 'deleteHome']); // Delete sect
 Route::get('fetch-home-data',[HomeController::class,'fetchHomeData']); // Fetch all home data 
 
 Route::post('/about', [AboutController::class, 'addOrUpdateAbout']); // Add or Update about
-Route::get('/about/{id?}', [AboutController::class, 'deleteAbout']); // Delete about
+Route::get('/about/{id?}', [AboutController::class, 'deleteAbout']); // Delete section about
 Route::get('fetch-about-data',[AboutController::class,'fetchAboutData']); // Fetch all about data 
 
+Route::post('/services',[ServicesController::class,'addOrUpdateServices']); // Add or Update services data
+Route::get('/services/{id?}',[ServicesController::class,'deleteServices']); // Delete section services
+Route::get('fetch-services-data',[servicesController::class,'fetchServicesData']); // Fetch all services data 
+
+Route::post('/usefull-linls', [UsefullLinlsController::class, 'addOrUpdateUsefullLinls']); // Add or Update usefull-linls
+Route::get('/usefull-linls/{id?}', [UsefullLinlsController::class, 'deleteUsefullLinls']); // Delete section usefull-linls
+Route::get('fetch-usefull-linls-data',[UsefullLinlsController::class,'fetchUsefullLinlsData']); // Fetch all usefull-linls data 
+
+Route::post('/latest-news', [LatestNewsController::class, 'addOrUpdateLatestNews']); // Add or Update latest-news
+Route::get('/latest-news/{id?}', [LatestNewsController::class, 'deleteLatestNews']); // Delete section latest-news
+Route::get('fetch-latest-news-data',[LatestNewsController::class,'fetchLatestNewsData']); // Fetch all latest-news data 
+
+Route::post('/contact', [ContactController::class, 'addOrUpdateContact']); // Add or Update Contact
