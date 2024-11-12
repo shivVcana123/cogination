edc02676f5ef4f33a6c494fb4e9426fe37619143
shiv kumar <shiv@vcanaglobal.com>
2024-11-12 11:22:34 +0530

    migration

---
+++ database/migrations/2024_11_06_104607_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('subject')->nullable();
+            $table->string('message')->nullable();
+            $table->string('title')->nullable();
+            $table->string('subtitle')->nullable();
+            $table->string('description')->nullable();
+            $table->string('button_content')->nullable();
+            $table->string('button_link')->nullable();
+            $table->string('image')->nullable();
+            $table->string('background_image')->nullable();
+            $table->string('background_color')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
